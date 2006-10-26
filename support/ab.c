0f94e99ab5b145b6ce6272ff2a2c33c96af86226
Tim Olsen <tolsen@limelabs.com>
2006-10-26 18:39:57 +0000

    remove svn:keywords property from apache2.2-svn/current
    git-svn-id: file:///srv/svn/limespot/vendor/apache2.2-svn/current@1307 a44d2f8d-3a13-463e-8d7a-201e96ec981d

--- support/ab.c
+++ support/ab.c
@@ -1730,14 +1730,14 @@ static void test(void)
 static void copyright(void)
 {
     if (!use_html) {
-        printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision$> apache-2.0");
+        printf("This is ApacheBench, Version %s\n", AP_AB_BASEREVISION " <$Revision: 1302 $> apache-2.0");
         printf("Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/\n");
         printf("Copyright 2006 The Apache Software Foundation, http://www.apache.org/\n");
         printf("\n");
     }
     else {
         printf("<p>\n");
-        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision$");
+        printf(" This is ApacheBench, Version %s <i>&lt;%s&gt;</i> apache-2.0<br>\n", AP_AB_BASEREVISION, "$Revision: 1302 $");
         printf(" Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/<br>\n");
         printf(" Copyright 2006 The Apache Software Foundation, http://www.apache.org/<br>\n");
         printf("</p>\n<p>\n");
